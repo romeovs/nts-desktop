09f85adada64b73a34b22ec5f2fba04b49b6ba53
Romeo Van Snick <romeovs@gmail.com>
2021-05-03 19:37:26 +0200

    Render channels

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -1,7 +1,33 @@
 import * as React from "react"
+import classnames from "classnames"
 
 import "./global.css"
 
+import { live } from "./lib/api"
+import { usePromise } from "./lib/use-promise"
+
+import { Splash } from "./splash"
+import { Channel } from "./channel"
+
+import css from "./app.module.css"
+
 export function App() {
-	return <div>Hello world</div>
+	const { loading, data, error } = usePromise(live)
+	const [channel, setChannel] = React.useState(0)
+
+	function toggle() {
+		setChannel(idx => (idx + 1) % 2)
+	}
+
+	return (
+		<>
+			<Splash hide={!loading} />
+			{data && (
+				<div className={classnames(css.channels, channel === 1 && css.channel2)} onClick={toggle}>
+					<Channel channel={1} info={data.channel1} />
+					<Channel channel={2} info={data.channel2} />
+				</div>
+			)}
+		</>
+	)
 }
