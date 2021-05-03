33264e0082c027f13e99306d7b76b233b8d5195c
Romeo Van Snick <romeovs@gmail.com>
2021-05-03 19:25:41 +0200

    Add channel component

---
+++ src/client/channel.tsx
@@ -0,0 +1,40 @@
+import * as React from "react"
+import type { ChannelInfo } from "./lib/api"
+
+import css from "./channel.module.css"
+
+type Props = {
+	info: ChannelInfo
+	channel: number
+}
+
+export function Channel(props: Props) {
+	const { info, channel } = props
+	const { name, image, starts, ends, location } = info
+
+	return (
+		<div className={css.channel}>
+			<img src={image} className={css.image} />
+			<div className={css.header}>
+				<div className={css.ch}>{channel}</div>
+				<div>
+					<div className={css.live}>
+						Live <span className={css.dot} />
+					</div>
+					<div className={css.times}>
+						{format(starts)} &mdash; {format(ends)}
+					</div>
+				</div>
+			</div>
+			<div className={css.footer}>
+				<div className={css.location}>{location}</div>
+				<br />
+				<span className={css.name}>{name}</span>
+			</div>
+		</div>
+	)
+}
+
+function format(date: Date): string {
+	return date.toLocaleTimeString("en-GB", { hour: "2-digit", minute: "2-digit" })
+}
