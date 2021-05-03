a63fd2c37532f30590531bdbdd3b86bd4fe669d8
Romeo Van Snick <romeovs@gmail.com>
2021-05-03 18:51:18 +0200

    Add Splash component

---
+++ src/client/splash.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import classnames from "classnames"
+
+import { Logo } from "./logo"
+import css from "./splash.module.css"
+
+type Props = {
+	hide: boolean
+}
+
+export function Splash(props: Props) {
+	const { hide } = props
+
+	return (
+		<div className={classnames(css.splash, hide && css.hide)}>
+			<Logo className={css.logo} />
+		</div>
+	)
+}
