6beec41e0fe05862d8609d4412580185eaeeaaa3
Romeo Van Snick <romeovs@gmail.com>
2021-05-04 18:22:05 +0200

    Add removeListener helpers to electron preload

--- src/client/electron.ts
+++ src/client/electron.ts
@@ -1,5 +1,7 @@
 interface Electron {
 	on(name: string, callback: (evt: Event, ...args: any[]) => void): void
+	removeListener(name: string, callback: (evt: Event, ...args: any[]) => void): void
+	removeAllListeners(name: string, callback: (evt: Event, ...args: any[]) => void): void
 }
 
 // @ts-expect-error
--- src/preload.js
+++ src/preload.js
@@ -4,4 +4,10 @@ contextBridge.exposeInMainWorld("electron", {
 	on: function (...arguments) {
 		return ipcRenderer.on(...arguments)
 	},
+	removeListener: function (...arguments) {
+		return ipcRenderer.removeListener(...arguments)
+	},
+	removeAllListeners: function (...arguments) {
+		return ipcRenderer.removeAllListeners(...arguments)
+	},
 })
