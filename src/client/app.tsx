8adfe02e10dc2ff56aab836a3571240e852cc440
Romeo Van Snick <romeovs@gmail.com>
2021-05-04 15:05:44 +0200

    Use hooks for api fetches in app

--- src/client/app.tsx
+++ src/client/app.tsx
@@ -4,8 +4,8 @@ import classnames from "classnames"
 import "./global.css"
 
 import { electron } from "./electron"
-import { live as getLive, Info } from "./lib/api"
-import { show as getShow, Show as ShowT } from "./lib/show"
+import { useLiveInfo } from "./lib/api"
+import { useShowInfo } from "./lib/show"
 
 import { Splash } from "./splash"
 import { Channel } from "./channel"
@@ -21,46 +21,19 @@ const streams = {
 	2: "https://stream-relay-geo.ntslive.net/stream2?client=NTSWebApp",
 }
 
-type State<T> = {
-	loading: boolean
-	data: T | null
-	error: Error | null
-}
-
 type Channel = 1 | 2 | "show"
 
 export function App() {
-	const [live, setLive] = React.useState<State<Info>>({ loading: true, error: null, data: null })
-	const [show, setShow] = React.useState<State<ShowT>>({ loading: false, error: null, data: null })
+	const live = useLiveInfo()
+	const show = useShowInfo()
 
 	const [index, setIndex] = React.useState<number>(1)
 	const [playing, setPlaying] = React.useState<Channel | null>(null)
 
 	React.useEffect(function () {
-		async function load() {
-			setLive({ loading: true, data: null, error: null })
-			try {
-				const data = await getLive()
-				setLive({ loading: false, data, error: null })
-			} catch (error) {
-				setLive({ loading: false, data: null, error })
-			}
-		}
-
-		load()
-
-		electron.on("open", load)
-		electron.on("drop", async function (evt: Event, url: string) {
-			setShow({ loading: true, data: null, error: null })
-			try {
-				const data = await getShow(url)
-				setShow({ loading: false, data, error: null })
-				setIndex(3)
-				setPlaying("show")
-			} catch (error) {
-				setShow({ loading: false, data: null, error })
-			}
-		})
+		live.load()
+		electron.on("open", () => live.load())
+		electron.on("drop", (_: Event, url: string) => show.load(url))
 	}, [])
 
 	function next() {
