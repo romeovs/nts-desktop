5b8fad36b6576f65c9623b15f93fef6c7938afb6
Romeo Van Snick <romeovs@gmail.com>
2021-05-05 14:11:37 +0200

    Add controls component

---
+++ src/client/controls.tsx
@@ -0,0 +1,84 @@
+import * as React from "react"
+import css from "./controls.module.css"
+
+import type { Show } from "./lib/show"
+
+import { PlayButton } from "./play"
+import { Mixcloud } from "./mixcloud"
+
+type Props = {
+	duration: number
+	position: number
+	show: Show
+	playing: boolean
+	onPlay: () => void
+	onStop: () => void
+	onSeek: (pos: number) => void
+}
+
+export function Controls(props: Props) {
+	const { show, duration, position, onPlay, onStop, playing, onSeek } = props
+	const [select, setSelect] = React.useState(0)
+	const width = (100 * position) / duration
+	const ref = React.useRef<HTMLDivElement | null>(null)
+
+	function handlePlayClick() {
+		if (playing) {
+			onStop()
+		} else {
+			onPlay()
+		}
+	}
+
+	function handleClick(evt: React.MouseEvent<HTMLDivElement>) {
+		if (!ref.current) {
+			return
+		}
+
+		const { left, width } = ref.current.getBoundingClientRect()
+		const x = evt.clientX - left
+		const percentage = x / width
+		const pos = Math.round(duration * percentage)
+		if (pos < position) {
+			return
+		}
+
+		onSeek(pos)
+	}
+
+	return (
+		<div className={css.controls}>
+			<PlayButton onClick={handlePlayClick} playing={playing} className={css.play} />
+			<div className={css.bar} onClick={handleClick} ref={ref}>
+				<div className={css.pos} style={{ width: `${width}%` }} />
+			</div>
+			<div className={css.time}>
+				{formatDuration(position)}/{formatDuration(duration)}
+			</div>
+		</div>
+	)
+}
+
+export function formatDuration(seconds: number): string {
+	const sec = seconds % 60
+	const min = ((seconds - sec) / 60) % 60
+	const hours = (seconds - sec - 60 * min) / 3600
+
+	const r = []
+	if (hours !== 0) {
+		r.push(hours)
+	}
+
+	r.push(pad(min))
+	r.push(pad(sec))
+
+	return r.join(":")
+}
+
+function pad(x: number): string {
+	if (x >= 10) {
+		return x.toString()
+	}
+
+	return `0${x}`
+}
