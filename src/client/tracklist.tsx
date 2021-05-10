97efc4b3d88e5fdf6c62c0783acfec042a216468
Romeo Van Snick <romeovs@gmail.com>
2021-05-10 10:40:20 +0200

    Add tracklist button component

---
+++ src/client/tracklist.tsx
@@ -0,0 +1,23 @@
+import * as React from "react"
+import { electron } from "./electron"
+import css from "./tracklist.module.css"
+
+type Props = {
+	channel: 1 | 2
+}
+
+export function Tracklist(props: Props) {
+	const { channel } = props
+
+	function handleClick() {
+		electron.send("tracklist", channel)
+	}
+
+	return (
+		<button type="button" onClick={handleClick} className={css.tracklist}>
+			<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+				<path d="M2.87 14.49h8V13h-8zm12-5h-12V11h12zm0-3.5h-12v1.5h12zm6.26 3.34l-2.88-1v7a2.67 2.67 0 01-2 2.61 2.53 2.53 0 01-2.88-1.44 2.73 2.73 0 01.71-3.24 2.49 2.49 0 013.2 0V6l3.84 1.33z" />
+			</svg>
+		</button>
+	)
+}
