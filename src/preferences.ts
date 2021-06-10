6e9be2b00ae506ec13bf386b84e3d39d8ac10fbb
Romeo Van Snick <romeovs@gmail.com>
2021-06-10 15:10:38 +0200

    Add helper to store preferences

---
+++ src/preferences.ts
@@ -0,0 +1,30 @@
+import path from "path"
+import { promises as fs } from "fs"
+import { app } from "electron"
+
+export type Preferences = {
+	volume: number
+}
+
+const defaults = {
+	volume: 0.8,
+}
+
+const filename = path.join(app.getPath("userData"), "preferences.json")
+
+export async function read(): Promise<Preferences> {
+	try {
+		const content = await fs.readFile(filename, "utf-8")
+		return JSON.parse(content)
+	} catch (err) {
+		return defaults
+	}
+}
+
+export async function write(preferences: Preferences): Promise<void> {
+	await fs.writeFile(filename, JSON.stringify(preferences))
+}
+
+export async function clear(): Promise<void> {
+	await fs.unlink(filename)
+}
