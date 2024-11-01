5558ba3348f899b20ae57954691756a98ef4f951
Romeo Van Snick <romeo@romeovansnick.be>
2024-11-01 17:06:57 +1100

    Clean up firebase code

---
+++ src/firebase/index.ts
@@ -0,0 +1,113 @@
+// @ts-expect-error
+import NTS from "./mod"
+import { collection, query, where, orderBy, limit, onSnapshot } from "firebase/firestore"
+
+const LIMIT = 15
+const store = NTS.firestore()
+
+export type LiveTrack = {
+	title: string
+	stream: 1 | 2 | null
+	artists: string[]
+	startTime: Date
+}
+
+function streamToPathname(stream: 1 | 2): string {
+	if (stream === 1) {
+		return "/stream"
+	}
+	if (stream === 2) {
+		return "/stream2"
+	}
+	return "unknown"
+}
+
+function pathnameToStream(pathname: string): 1 | 2 | null {
+	if (pathname === "/stream") {
+		return 1
+	}
+	if (pathname === "/stream2") {
+		return 2
+	}
+	return null
+}
+
+type Handler = (err: Error | null, res: LiveTrack[] | null) => void
+
+function liveTracks(stream: 1 | 2, fn: Handler): () => void {
+	const qry = query(
+		collection(store, "live_tracks"),
+		where("stream_pathname", "==", streamToPathname(stream)),
+		orderBy("start_time", "desc"),
+		limit(LIMIT),
+	)
+
+	return onSnapshot(
+		qry,
+		function (snapshot) {
+			const res: LiveTrack[] = []
+			snapshot.forEach(function (doc) {
+				const data = doc.data()
+				res.push({
+					title: data.song_title,
+					artists: data.artist_names,
+					stream: pathnameToStream(data.stream_pathname),
+					startTime: data.start_time.toDate(),
+				})
+			})
+			fn(null, res)
+		},
+		function (err) {
+			fn(err, null)
+		},
+	)
+}
+
+let promise: Promise<void> | null = null
+
+export async function login(email: string, password: string) {
+	if (!promise) {
+		NTS._app()
+		promise = NTS.signIn(email, password)
+	}
+
+	return promise
+}
+
+import { useEffect, useState } from "react"
+
+function useLogin(username: string, password: string) {
+	const [loggedIn, setLoggedIn] = useState(false)
+	useEffect(
+		function () {
+			if (!username || !password) {
+				return
+			}
+
+			login(username, password).then(() => setLoggedIn(true))
+		},
+		[username, password],
+	)
+	return loggedIn
+}
+
+export function useLiveTracks(email: string, password: string, stream: 1 | 2): LiveTrack[] {
+	const loggedIn = useLogin(email, password)
+	const [tracks, setTracks] = useState<LiveTrack[]>([])
+
+	useEffect(
+		function () {
+			if (!loggedIn) {
+				return
+			}
+			return liveTracks(stream, function (err, res) {
+				if (err) {
+					return
+				}
+				setTracks(res ?? [])
+			})
+		},
+		[stream, loggedIn],
+	)
+	return tracks
+}
