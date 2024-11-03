d8415579a31f862b6b30dc4f235de163177d7e5f
Romeo Van Snick <romeo@romeovansnick.be>
2024-11-04 10:46:45 +1100

    Set up tsconfig paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import tspaths from "vite-tsconfig-paths"
 
 export default defineConfig({
 	build: {
 		manifest: true,
 		outDir: "./dist/client",
 	},
-	plugins: [react()],
+	plugins: [react(), tspaths()],
 })
